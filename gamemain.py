562913edc9b61cbbc1b44755908f33107df2fcf9
turtledude16 <turtledude1600@gmail.com>
2023-03-14 16:30:23 -0500

    added moving platforms

--- gamemain.py
+++ gamemain.py
@@ -79,11 +79,18 @@ class platform(pygame.sprite.Sprite):
         super().__init__()
         self.surf = pygame.Surface((random.randint(50, 100), 12))
         self.surf.fill((255, 0, 0))
-        self.rect = self.surf.get_rect(center = (random.randint(0, WIDTH-10), random.randint(0, HEIGHT-30)))
+        self.rect = self.surf.get_rect(center = (random.randint(0, WIDTH-10), random.randint(0, HEIGHT-40)))
         self.point = True
+        self.speed = random.randint(-1, 1)
+        self.moving = True
 
     def move(self):
-        pass
+        if self.moving == True:
+            self.rect.move_ip(self.speed, 0)
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
 
 def check(platform, groupies):
     if pygame.sprite.spritecollideany(platform,groupies):
@@ -113,6 +120,7 @@ def plat_gen():
 PT1 = platform()
 P1 = Player()
 PT1.point = False
+PT1.moving = False
 #Draw sprites
 PT1.surf = pygame.Surface((WIDTH, 20))
 PT1.surf.fill((255, 0, 0))
